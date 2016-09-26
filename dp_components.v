78ee73bdc63bd9a3b9be27fa66fd9050914f6b83
meenuh <dearmeenuh@gmail.com>
2016-09-25 19:27:16 -0700

    implementing components for dp

--- dp_components.v
+++ dp_components.v
@@ -1,23 +1,62 @@
-module counter();
-
+module counterA(addr, inc, reset, clk);
+    output reg [2:0] addr;
+    input inc, reset, clk;
+    
+    always @(posedge clk) begin
+        if(reset) addr = 3'b0;
+        else addr = addr + 3'b1;
+    end
 endmodule
 
-module dreg();
+module counterB(addr, inc, reset, clk);
+    output reg [1:0] addr;
+    input inc, reset, clk;
+    
+    always @(posedge clk) begin
+        if(reset) addr = 2'b0;
+        else addr = addr + 2'b1;
+    end
+endmodule
 
+module dreg(q, d, clk);
+    output reg [7:0] q;
+    input [7:0] d;
+    input clk;
+    
+    always @(posedge clk) begin
+        q = d;
+    end
 endmodule
 
 module comparator();
 
 endmodule
 
-module adder();
-
+module adder(addOut, in1, in2);
+    output [7:0] addOut;
+    input [7:0] in1;
+    input [7:0] in2;
+    
+    assign addOut = in1 + in2;
 endmodule
 
-module sub();
-
+module sub(subOut, in1, in2);
+    output [7:0] subOut;
+     input [7:0] in1;
+     input [7:0] in2;
+     
+     assign subOut = in1 - in2;
 endmodule
 
-module mux2to1();
-
+module mux2to1(dataOut, addIn, subIn, sel);
+    output reg [7:0] dataOut;
+    input [7:0] addIn;
+    input [7:0] subIn;
+    input sel;
+    
+    always @(addIn, subIn, sel) begin
+        if(sel) dataOut = subIn;
+        else dataOut = addIn;
+    end
+    
 endmodule
