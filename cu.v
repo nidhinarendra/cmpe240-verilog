d7de2b74f86f79d82214516d0add9185e27004f1
meenuh <dearmeenuh@gmail.com>
2016-09-25 18:34:32 -0700

    fixing reset values

--- cu.v
+++ cu.v
@@ -1,8 +1,10 @@
 module cu(clk, rst, WEA, IncA, IncB, WEB);
     input clk, rst, WEA, IncA, IncB, WEB;
     reg [4:0] count;
+    reg reset; //need this to AND with input rst signal and count values
+    
     //instantiate up_counter count, rst, clk
-    up_counter m1(count, rst, clk);
+    up_counter m1(count, reset, clk);
     //generate WEA
 
     //generate IncA
